e38ec9bd23552b826622441f293a750247a182d8
uptSmart <uptSmart@163.com>
2022-10-24 18:13:54 +0800

    change the gas from 250000 to 300000

--- src/views/components/OperationModal/index.vue
+++ src/views/components/OperationModal/index.vue
@@ -286,7 +286,7 @@ export default {
       fee: '900',
       feeDenom: '',
       wallet: 'ledgerUSB',
-      gas: '250000',
+      gas: '300000',
       memo: '',
       blockingMsg: this.address ? 'You are not the owner' : 'No available account found.',
       actionName: 'Send',
